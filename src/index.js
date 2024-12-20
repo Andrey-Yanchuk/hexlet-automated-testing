fc3cedc373444cef2ea9a4c5d1e7f101f7ff2c77
Yanchuk Andrey <orpiy@mail.ru>
2024-12-21 00:22:19 +0700

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-// index.js
+// src/index.js
 /*-----------------------------------------------------*/
 /*-----------------------------------------------------*/
 export const capitalize = (text) => {
+    if (text === null || text === undefined || text === '') return '';
     const [firstChar, ...restSubstring] = text;
     return `${firstChar.toUpperCase()}${restSubstring.join('')}`;
 };
-console.log(capitalize('andrey'));
+/*-----------------------------------------------------*/
+export const get = (obj, key, defaultValue) => {
+    // if (Object.keys(obj).length === 0) return defaultValue;
+    // const result = Object.entries(obj).map(([objKey, objValue]) => {
+    //     if (objKey === key) return objValue;
+    //     return undefined;
+    // });
+    // return result !== undefined ? result : defaultValue; // вернет массив со строкой, а не саму строку, второй вариант решение более правильный и лаконичнее
+    if (Object.keys(obj).includes(key)) return obj[key];
+    return defaultValue;
+};
+/*-----------------------------------------------------*/

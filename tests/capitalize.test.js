6a0104eb698c65a8a4d82afa3d0671e29f33eadc
Yanchuk Andrey <orpiy@mail.ru>
2024-12-21 00:22:37 +0700

    update tests/capitalize.test.js

---
+++ tests/capitalize.test.js
@@ -0,0 +1,13 @@
+// tests/capitalize.test.js
+import { capitalize } from "../src/index.js"
+/*-----------------------------------------------------*/
+if (capitalize('hello') !== 'Hello') {
+    throw new Error('Функция работает неверно!');
+}
+if (capitalize('') !== '') {
+    throw new Error('Функция работает неверно!');
+}
+if (capitalize(null) == null) {
+    throw new Error('Функция работает неверно!');
+}
+console.log('Все тесты пройдены!');

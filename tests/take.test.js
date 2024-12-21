8667030487239b55dba3ab506e0d00891ecc6103
Yanchuk Andrey <orpiy@mail.ru>
2024-12-21 19:47:36 +0700

    add tests/take.test.js

---
+++ tests/take.test.js
@@ -0,0 +1,9 @@
+// tests/take.test.js
+import { take } from '../src/index.js';
+import { strict as assert } from 'assert'
+/*-----------------------------------------------------*/
+assert.deepEqual(take([], 2), [], 'AssertionError:'); // проверяет, что пустой массив остаётся пустым независимо от n
+assert.deepEqual(take([1, 2, 3]), [1], 'AssertionError:'); // проверяет поведение с массивом, если n не указан (по умолчанию n = 1)
+assert.deepEqual(take([1, 2, 3], 2), [1, 2], 'AssertionError:'); // проверяет возврат первых двух элементов
+assert.deepEqual(take([4, 3], 9), [4, 3], 'AssertionError:'); // проверяет, что если n больше длины массива, возвращается весь массив
+console.log("Все тесты пройдены!");

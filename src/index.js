1bae4c61312542c0d697854315e262eb8fb7bab5
Yanchuk Andrey <orpiy@mail.ru>
2024-12-21 19:49:11 +0700

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@
 /*-----------------------------------------------------*/
 /*-----------------------------------------------------*/
 export const capitalize = (text) => {
-    if (text === null || text === undefined || text === '') return '';
-    const [firstChar, ...restSubstring] = text;
-    return `${firstChar.toUpperCase()}${restSubstring.join('')}`;
+  if (text === null || text === undefined || text === "") return "";
+  const [firstChar, ...restSubstring] = text;
+  return `${firstChar.toUpperCase()}${restSubstring.join("")}`;
 };
 /*-----------------------------------------------------*/
 export const get = (obj, key, defaultValue) => {
-    // if (Object.keys(obj).length === 0) return defaultValue;
-    // const result = Object.entries(obj).map(([objKey, objValue]) => {
-    //     if (objKey === key) return objValue;
-    //     return undefined;
-    // });
-    // return result !== undefined ? result : defaultValue; // вернет массив со строкой, а не саму строку, второй вариант решение более правильный и лаконичнее
-    if (Object.keys(obj).includes(key)) return obj[key];
-    return defaultValue;
+  // if (Object.keys(obj).length === 0) return defaultValue;
+  // const result = Object.entries(obj).map(([objKey, objValue]) => {
+  //     if (objKey === key) return objValue;
+  //     return undefined;
+  // });
+  // return result !== undefined ? result : defaultValue; // вернет массив со строкой, а не саму строку, второй вариант решение более правильный и лаконичнее
+  if (Object.keys(obj).includes(key)) return obj[key];
+  return defaultValue;
 };
+/*-----------------------------------------------------*/
+export const take = (items, n = 1) => items.slice(0, n);
 /*-----------------------------------------------------*/

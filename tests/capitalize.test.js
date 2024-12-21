5414944c1a77d546067d6f2b25c026974c906b95
Yanchuk Andrey <orpiy@mail.ru>
2024-12-21 19:49:53 +0700

    update tests/capitalize.test.js

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,13 +1,22 @@
 // tests/capitalize.test.js
-import { capitalize } from "../src/index.js"
+import { capitalize } from "../src/index.js";
+import { strict as assert } from 'assert'
 /*-----------------------------------------------------*/
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-}
-if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-}
-if (capitalize(null) == null) {
-    throw new Error('Функция работает неверно!');
-}
-console.log('Все тесты пройдены!');
+// Before
+// if (capitalize("hello") !== "Hello") {
+//   throw new Error("Функция работает неверно!");
+// }
+// if (capitalize("") !== "") {
+//   throw new Error("Функция работает неверно!");
+// }
+// if (capitalize(null) !== "") {
+//   throw new Error("Функция null работает неверно!");
+// }
+// console.log("Все тесты пройдены!");
+/*-----------------------------------------------------*/
+// After
+assert.equal(capitalize("hello"), "Hello", "Функция работает неверно!");
+assert.equal(capitalize("Hello"), "Hello", "Функция работает неверно!");
+assert.equal(capitalize(""), "", "Функция работает неверно!");
+assert.equal(capitalize(null), "", "Функция работает неверно!");
+console.log("Все тесты пройдены!");

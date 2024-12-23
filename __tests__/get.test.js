94cfb599737e13aebbb927055b1672dd017b2a5d
Yanchuk Andrey <orpiy@mail.ru>
2024-12-23 20:36:17 +0700

    update __tests__/get.test.js

--- __tests__/get.test.js
+++ __tests__/get.test.js
@@ -1,13 +1,21 @@
 // tests/get.test.js
 import { get } from "../src/index.js";
 /*-----------------------------------------------------*/
-if (get({ hello: "world" }, "hello") !== "world") {
-  throw new Error("Функция работает неверно!");
-}
-if (get({ hello: "world" }, "hello", "kitty") !== "world") {
-  throw new Error("Функция работает неверно!");
-}
-if (get({}, "hello", "kitty") !== "kitty") {
-  throw new Error("Функция работает неверно!");
-}
-console.log("Все тесты пройдены!");
+// Use throw Error
+// if (get({ hello: "world" }, "hello") !== "world") {
+//   throw new Error("Функция работает неверно!");
+// }
+// if (get({ hello: "world" }, "hello", "kitty") !== "world") {
+//   throw new Error("Функция работает неверно!");
+// }
+// if (get({}, "hello", "kitty") !== "kitty") {
+//   throw new Error("Функция работает неверно!");
+// }
+// console.log("Все тесты пройдены!");
+/*-----------------------------------------------------*/
+// Use jest
+test('get', () => {
+    expect(get({ hello: "world" }, "hello")).toEqual('world');
+    expect(get({ hello: "world" }, "hello", "kitty")).toEqual('world');
+    expect(get({}, "hello", "kitty")).toEqual('kitty');
+});

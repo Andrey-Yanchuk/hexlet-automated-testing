cfa0aa7b3b240b9a84733120f8a658a261159321
Yanchuk Andrey <orpiy@mail.ru>
2024-12-23 20:36:44 +0700

    update __tests__/indexOf.test.js

--- __tests__/indexOf.test.js
+++ __tests__/indexOf.test.js
@@ -2,7 +2,15 @@
 import { indexOf } from "../src/index.js";
 import { strict as assert } from "assert";
 /*-----------------------------------------------------*/
-assert.equal(indexOf([1, 2, 1, 2], 2), 1);
-assert.equal(indexOf([1, 2, 1, 2], 2, 2), 3);
-assert.equal(indexOf([2, "one", "cat", false], 8), -1);
-console.log("Все тесты пройдены!");
+// Use assert
+// assert.equal(indexOf([1, 2, 1, 2], 2), 1);
+// assert.equal(indexOf([1, 2, 1, 2], 2, 2), 3);
+// assert.equal(indexOf([2, "one", "cat", false], 8), -1); 
+// console.log("Все тесты пройдены!");
+/*-----------------------------------------------------*/
+// Use jest
+test('indexOf', () => {
+    expect(indexOf([1, 2, 1, 2], 2)).toEqual(1);
+    expect(indexOf([1, 2, 1, 2], 2, 2)).toEqual(3);
+    expect(indexOf([2, "one", "cat", false], 8)).toEqual(-1);
+});

7eec9c33f03e47502f8bd04242ef7a023d361804
Yanchuk Andrey <orpiy@mail.ru>
2024-12-25 23:18:37 +0700

    update __tests__/gt.test.js

--- __tests__/gt.test.js
+++ __tests__/gt.test.js
@@ -2,7 +2,11 @@
 import { gt } from "../src/index.js";
 /*-----------------------------------------------------*/
 test("gt", () => {
-  expect(gt(3, 1)).toBe(true); //true
-  expect(gt(3, 3)).toBe(false); //false
-  expect(gt(1, 3)).toBe(false); //false
+  expect(gt(3, 1)).toBe(true); // true
+  expect(gt(3, 3)).toBe(false); // false
+  expect(gt(1, 3)).toBe(false); // false
+  expect(gt(-1, -3)).toBe(true); // true
+  expect(gt(-3, -1)).toBe(false); // false
+  expect(gt(0, -1)).toBe(true); // true
+  expect(gt(0, 1)).toBe(false); // false
 });

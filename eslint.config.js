4d3364196451928be410607749f1e79f694c93d4
Yanchuk Andrey <orpiy@mail.ru>
2025-01-07 23:53:51 +0700

    update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
+    ignores: ["coverage/**"],
     languageOptions: {
       globals: {
         ...globals.browser,
